d2fc25b7b5bcf10aed60c077766d17d29977b2a5
Tevfik istanbullu <tevfikistanbullu94@gmail.com>
2023-08-08 16:35:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import LoginSignup from "./pages/LoginSignup";
+import Notes from "./pages/Notes";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LoginSignup />,
+    },
+    {
+      path: "/notes",
+      element: <Notes />,
+    },
+  ]);
   return (
     <div className="App">
-      <h1>Notes</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }

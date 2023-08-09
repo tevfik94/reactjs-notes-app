1134fda92ffed1c6fe1166f2888b9fbd0923a7df
Tevfik istanbullu <tevfikistanbullu94@gmail.com>
2023-08-09 16:29:40 +0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/Navbar.css";
+import { FiDownload, FiLogOut } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import LogoutPopup from "./LogoutPopup";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const navigate = useNavigate();
+  const [showPopup, setShowPopup] = useState(false);
+  const handleLogout = () => {
+    setShowPopup(true);
+  };
+  const handleLogoutConfirmed = async () => {
+    try {
+      const response = await fetch(
+        "https://ubade.pythonanywhere.com/api/logout",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+      if (response.ok) {
+        localStorage.removeItem("token");
+        navigate("/");
+      } else {
+        throw new Error("Logout faild");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    setShowPopup(false);
+  };
+  const handleCancel = () => {
+    setShowPopup(false);
+  };
+
+  return (
+    <div className="navbar">
+      <h1>Notes App</h1>
+      <ul className="navbar-links">
+        <li>
+          <FiDownload title="Download notes" />
+        </li>
+        <li>
+          <FiLogOut title="Logout" onClick={handleLogout} />
+        </li>
+      </ul>
+      {showPopup && (
+        <LogoutPopup
+          onCancel={handleCancel}
+          onLogoutConfirmed={handleLogoutConfirmed}
+        />
+      )}
+    </div>
+  );
 };
 
 export default NavBar;

2ff551440aef0019eaa8cea0939af64b85538427
Tevfik istanbullu <tevfikistanbullu94@gmail.com>
2023-08-13 14:51:14 +0300

    Update LoginSignup.jsx

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -11,14 +11,14 @@ const LoginSignup = () => {
   const [isActive, setIsActive] = useState(false);
   const [error, setError] = useState(null);
   const navigateToNotes = () => {
-    navigate("/reactjs-notes-app/notes");
+    navigate("/notes");
   };
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
       navigateToNotes();
     }
-  }); // Empty dependency array ensures this effect runs once on component mount
+  }, []); // Empty dependency array ensures this effect runs once on component mount
 
   const handleRegisterClick = () => {
     setIsActive(true);
@@ -86,7 +86,7 @@ const LoginSignup = () => {
 
     // Store token in local storage
     localStorage.setItem("token", token);
-    navigate("/reactjs-notes-app/notes");
+    navigate("/notes");
   }
   async function handleSubmitLogin(e) {
     e.preventDefault();
@@ -109,7 +109,7 @@ const LoginSignup = () => {
       if (response.ok) {
         const { token } = await response.json();
         localStorage.setItem("token", token);
-        navigate("/reactjs-notes-app/notes");
+        navigate("/notes");
       } else {
         setError("Invalid username or password. Please try again.");
       }

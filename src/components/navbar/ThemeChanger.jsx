d8bc1a0add2833adeaae21c114f128f69a611a7d
Tevfik istanbullu <tevfikistanbullu94@gmail.com>
2023-08-27 16:19:05 +0300

    Create ThemeChanger.jsx

---
+++ src/components/navbar/ThemeChanger.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { RiSunFill } from "react-icons/ri";
+
+const ThemeChanger = () => {
+  return (
+    <div>
+      <RiSunFill />
+    </div>
+  );
+};
+
+export default ThemeChanger;

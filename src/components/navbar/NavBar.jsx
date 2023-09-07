aebc8e6a58b887c95526c8d9dd13bcb2c64dc6bd
Tevfik istanbullu <tevfikistanbullu94@gmail.com>
2023-09-07 15:03:25 +0300

    Update NavBar.jsx

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -3,21 +3,22 @@ import "../../styles/Navbar.css";
 import ExportNotes from "./ExportNotes";
 import Logout from "./Logout";
 import ThemeChanger from "./ThemeChanger";
+import SearchBar from "./SearchBar";
 
-const NavBar = () => {
+const NavBar = ({ onSearch, notes }) => {
   return (
     <div className="navbar">
       <h1>Notes App</h1>
       <ul className="navbar-links">
         <li>
-          <input type="text" />
+          <SearchBar onSearch={onSearch} />
         </li>
         <li>
           <ThemeChanger />
         </li>
 
         <li>
-          <ExportNotes />
+          <ExportNotes notes={notes} />
         </li>
         <li>
           <Logout />

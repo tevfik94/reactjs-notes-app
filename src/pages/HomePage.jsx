4c323881a317a8a9955cb6d958a8e202cd0bb7ed
Tevfik istanbullu <tevfikistanbullu94@gmail.com>
2023-09-07 15:03:14 +0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../components/navbar/NavBar";
 import Footer from "../components/footer/Footer";
 import Notes from "./Notes";
 import "../styles/HomePage.css";
 
 const HomePage = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [notes, setNotes] = useState([]); // Initialize your notes state here
+
+  const handleSearch = (searchTerm) => {
+    // Implement filtering logic here
+    // For example, you can use the `filter` method to filter notes
+    const filteredNotes = notes.filter((note) =>
+      note.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setNotes(filteredNotes);
+    setSearchTerm(searchTerm); // Update the search term state
+  };
+
   return (
     <div className="homepage">
-      <NavBar />
-      <Notes />
+      <NavBar searchTerm={searchTerm} onSearch={handleSearch} />
+      <Notes notes={notes} searchTerm={searchTerm} />
       <Footer />
     </div>
   );

3bd949ae977c9ead7bac4dd6446f88a101822c9c
Tevfik istanbullu <tevfikistanbullu94@gmail.com>
2023-09-10 14:00:25 +0300

    Update ThemeContext.jsx

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,16 +1,23 @@
 // ThemeContext.js
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 // Create a context
 const ThemeContext = createContext();
 
 // Create a provider component
 export const ThemeProvider = ({ children }) => {
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const toggleTheme = () => {
     setIsDarkMode(!isDarkMode);
   };
+  // Set the theme based on the user's preference when the component mounts
+  useEffect(() => {
+    setIsDarkMode(prefersDarkMode);
+  }, [prefersDarkMode]);
 
   return (
     <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
